3b1d83f42624334ae87ac23d733c008af9ccf780
ambikapathak97 <72973284+ambikapathak97@users.noreply.github.com>
2022-04-17 12:56:15 +0530

    Add files via upload
    This is the program for the area of circle with radius 1.1.

---
+++ Area of Circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import math as m
+pi=np.pi
+r=1.1
+A=pi*r**2
+print("The area of the given circle is", A)
+
+
+# In[ ]:
+
+
+
+
